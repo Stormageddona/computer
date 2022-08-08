e7f7878f3f2ff1f5d6bc13bde6eb0629c994ca73
GYB <gsb0708@naver.com>
2022-08-08 14:36:04 +0900

    회원가입 완료

--- src/main/webapp/assets/js/account/join.js
+++ src/main/webapp/assets/js/account/join.js
@@ -3,22 +3,23 @@ $(function()
     $(".join_btn").click(function()
     {
         if (!confirm("가입 하시겠습니까?")) return ;
-join_id
-join_pwd
-join_name
-join_phone
-join_birth
         let data = 
             {
-                aci_id:$("join_id").val(),
+                aci_id:$(".join_id").val(),
                 aci_pwd:$(".join_pwd").val(),
                 aci_name:$(".join_name").val(),
+                aci_nickname:$(".join_nickname").val(),
                 aci_phone:$(".join_phone").val(),
                 aci_birth:makeDateString(new Date($(".join_birth").val()))
             }
+        console.log(data)
         $.ajax
         ({
-            url:"/api/account/join",type:"put", data:JSON.stringify(data), conten
+            url:"/api/account/join",type:"put", data:JSON.stringify(data), contentType:"application/json",
+            success:function(result)
+            {
+                alert(result.message) ;
+            }
         })
     })
 })
