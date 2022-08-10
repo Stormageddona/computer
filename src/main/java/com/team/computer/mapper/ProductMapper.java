9ef05fa8ca9c13f7d0d9c9bbebb21b7ec1700f3c
GYB <gsb0708@naver.com>
2022-08-10 11:03:00 +0900

    상품 페이지 작업 및 백엔드 정리

--- src/main/java/com/team/computer/mapper/ProductMapper.java
+++ src/main/java/com/team/computer/mapper/ProductMapper.java
@@ -3,6 +3,7 @@ package com.team.computer.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 
 import com.team.computer.data.CaseInfoVO;
 import com.team.computer.data.CoolerInfoVO;
@@ -11,9 +12,8 @@ import com.team.computer.data.GpuInfoVO;
 
 @Mapper
 public interface ProductMapper {
-    public List<CaseInfoVO> selectCaseListDesc(Integer offset, String keyword);
-    public List<CaseInfoVO> selectCaseListAsc(Integer offset, String keyword);
-    public Integer selectCaseListCnt(String keyword);
+    public List<CaseInfoVO> selectCaseList(Integer offset, String keyword, Boolean desc);
+    public Integer selectCaseListCnt(Integer offset, String keyword);
 
     public List<CoolerInfoVO> selectCoolerListDesc(Integer offset, String keyword);
     public List<CoolerInfoVO> selectCoolerListAsc(Integer offset, String keyword);
@@ -26,4 +26,7 @@ public interface ProductMapper {
     public List<GpuInfoVO> selectGpuListDesc(Integer offset, String keyword);
     public List<GpuInfoVO> selectGpuListAsc(Integer offset, String keyword);
     public Integer selectGpuListCnt(String keyword);
+
+    // //테스트
+    public void intsertTestCase(CaseInfoVO data) ;
 }
