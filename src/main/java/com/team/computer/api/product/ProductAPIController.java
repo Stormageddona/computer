127535a3a628179399e2ed00914e21fd17641b60
Stormageddona <gsb0708@naver.com>
2022-08-17 00:50:05 +0900

    자동화 작업 완료

--- src/main/java/com/team/computer/api/product/ProductAPIController.java
+++ src/main/java/com/team/computer/api/product/ProductAPIController.java
@@ -1,6 +1,5 @@
 package com.team.computer.api.product;
 
-import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -13,7 +12,6 @@ import org.springframework.lang.Nullable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/team/computer/mapper/AccountMapper.java
+++ src/main/java/com/team/computer/mapper/AccountMapper.java
@@ -1,6 +1,5 @@
 package com.team.computer.mapper;
 
-import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
